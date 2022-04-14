90157c8c5b5bfcdd15b6eb521ad36dfbd8597f0d
Joe Deering <JDeering94@gmail.com>
2022-04-14 17:32:54 +0100

    added artist model

--- songs/models.py
+++ songs/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
+class Artist (models.Model):
+  name = models.CharField(max_length=50)
+  def __str__(self):
+    return self.name
+
 class Song (models.Model):
   name = models.CharField(max_length=200)
   # album = models.CharField(max_length=100)
-  # artist = models.CharField(max_length=100)
+  artist = models.ForeignKey(Artist, related_name='songs', max_length=50, on_delete=models.CASCADE)
+  year = models.IntegerField()
   description = models.CharField(max_length=300)
   spotify_link = models.CharField(max_length=200)
 
   def __str__(self):
     return self.name
-
-
-
