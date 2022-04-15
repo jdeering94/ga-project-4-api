dc19284eaa4fe4b887c537e7337e51ec1864e9bd
Joe Deering <JDeering94@gmail.com>
2022-04-15 12:46:42 +0100

    added film and album models and their relationships

--- songs/admin.py
+++ songs/admin.py
@@ -3,3 +3,5 @@ from .models import *
 
 admin.site.register(Song)
 admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Film)
--- songs/models.py
+++ songs/models.py
@@ -5,9 +5,20 @@ class Artist (models.Model):
   def __str__(self):
     return self.name
 
+class Album (models.Model):
+  name = models.CharField(max_length=100)
+  image = models.CharField(max_length=200)
+
+class Film (models.Model):
+  title = models.CharField(max_length=50)
+  Year = models.IntegerField()
+  Director = models.CharField(max_length=50)
+  image = models.CharField(max_length=200)
+
 class Song (models.Model):
   name = models.CharField(max_length=200)
-  # album = models.CharField(max_length=100)
+  films = models.ManyToManyField(Film, related_name='songs', blank=True)
+  album = models.ForeignKey(Album, related_name='songs', max_length=100, on_delete=models.CASCADE)
   artist = models.ForeignKey(Artist, related_name='songs', max_length=50, on_delete=models.CASCADE)
   year = models.IntegerField()
   description = models.CharField(max_length=300)
