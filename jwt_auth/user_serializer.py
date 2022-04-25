5f3c9d1ec0600e37deff8abb0571d6bd4480dd59
Joe Deering <JDeering94@gmail.com>
2022-04-26 00:04:36 +0100

    added reviews to user serializer

--- jwt_auth/user_serializer.py
+++ jwt_auth/user_serializer.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from reviews.serializers import PopulatedReviewSerializer
 from songs.serializers.common import PopulatedSongSerializer
 
 User = get_user_model()
 
 class CustomUserSerializer(serializers.ModelSerializer):
   liked_songs = PopulatedSongSerializer(many=True)
+  reviews = PopulatedReviewSerializer(many=True)
   class Meta:
     model = User
-    fields = ('username', 'image', 'liked_songs')
+    fields = ('username', 'image', 'liked_songs', 'reviews')
